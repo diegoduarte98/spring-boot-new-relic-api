e78224bd5027d930995e9231e87d1a586ecc1b01
Diego Duarte <diegoduarte98@gmail.com>
2021-05-18 23:48:22 -0300

    inserts

--- src/main/java/br/com/diegoduarte/springbootnewrelicapi/NewRelicController.java
+++ src/main/java/br/com/diegoduarte/springbootnewrelicapi/NewRelicController.java
@@ -21,16 +21,28 @@ public class NewRelicController {
 	@GetMapping
 	public void get() {
 
-		final Map<String, Object> map = new HashMap<>();
-		map.put("status", "APROVADO");
-		map.put("TotalVendas", repository.countByStatus("APROVADO"));
+//        Map eventAttributes = new HashMap();
+//        eventAttributes.put("animalType", "cat");
+//        eventAttributes.put("animalCount", 50);
+//        NewRelic.getAgent().getInsights().recordCustomEvent("animalEvent", eventAttributes);
+//
+//        eventAttributes.clear();
+//
+//        eventAttributes.put("animalType", "dog");
+//        eventAttributes.put("animalCount", 15);
+//        NewRelic.getAgent().getInsights().recordCustomEvent("animalEvent", eventAttributes);
+//		  select sum(animalCount) from animalEvent facet animalType since 1 day ago
+//        select sum(TotalVendas) from StatusVendas facet status since 1 day ago
+		final Map map = new HashMap();
+		map.put("status", "ATIVO");
+		map.put("TotalVendas", repository.countByStatus("ATIVO"));
 
 		NewRelic.getAgent().getInsights().recordCustomEvent("StatusVendas", map);
 
 		map.clear();
 
-		map.put("status", "RECUSADO");
-		map.put("TotalVendas", repository.countByStatus("RECUSADO"));
+		map.put("status", "BLOQUEADO");
+		map.put("TotalVendas", repository.countByStatus("BLOQUEADO"));
 
 		NewRelic.getAgent().getInsights().recordCustomEvent("StatusVendas", map);
 	}
